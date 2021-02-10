1ae5b0521b494903c5006df53e3c0cb00940f718
Zazan Pratama <mail@zazan.me>
2021-02-10 10:35:07 +0700

    another update

---
+++ dist/_nuxt/static/1612926579/person/1321070/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612926579",layout:"no-footer",error:{message:"Page not found",statusCode:500},state:{search:{searchOpen:a,fromPage:"\u002F"}},serverRendered:a,routePath:"\u002Fperson\u002F1321070",config:{}}}(false));
---
+++ dist/_nuxt/static/1612926579/person/2380748/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612926579",layout:"no-footer",error:{message:"This person is not available",statusCode:500},state:{search:{searchOpen:a,fromPage:"\u002F"}},serverRendered:a,routePath:"\u002Fperson\u002F2380748",config:{}}}(false));
